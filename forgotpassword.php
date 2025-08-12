db2875f882bbe567b1b481a76cfd056ba1c0f525
Norisah <norisah@motac.gov.my>
2025-08-12 12:19:11 +0800

    latest change

---
+++ forgotpassword.php
@@ -0,0 +1,6 @@
+<!-- forgot_password.php -->
+<form action="send_reset_link.php" method="POST">
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    <button type="submit">Hantar Link Reset</button>
+</form>
